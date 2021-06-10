3b452e05dbfd618ae19bc9fe19ebf48007ebfbbf
ObedBalogun <b.obed@yahoo.com>
2021-06-10 11:24:57 +0100

    initial commit

--- akmarv_backend/urls.py
+++ akmarv_backend/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
@@ -23,4 +25,10 @@ urlpatterns = [
     path('api/client/', include('beat.urls')),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
