6a731b0a75cdb43227ca1f96093c448fe292ec87
ObedBalogun <b.obed@yahoo.com>
2021-06-17 23:56:01 +0100

    Webpack Test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 module.exports = {
-    entry: "./index.js",
+    entry: "./src/index.js",
     output: {
         path: path.join(__dirname, '/dist', '/public'),
         filename: "index.bundle.js",
