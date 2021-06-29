964044e614d1f9d39ab205b0e56f063ecff1e586
ObedBalogun <b.obed@yahoo.com>
2021-06-29 02:29:09 +0100

    License display edit

--- akmarv_backend/settings.py
+++ akmarv_backend/settings.py
@@ -69,18 +69,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://localhost:3010',
-    'https://paystack.com',
-)
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -89,6 +84,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'akmarv_backend.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
     {
