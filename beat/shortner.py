033601eade7ece16a8b05107c43155f24b7b7287
ObedBalogun <b.obed@yahoo.com>
2021-07-04 09:56:05 +0100

    Audio Test

---
+++ beat/shortner.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+from decouple import config
+
+
+def shortener(url):
+    linkRequest = {
+        "destination": url
+        , "domain": {"fullName": "rebrand.ly"}
+        # , "slashtag": "A_NEW_SLASHTAG"
+        # , "title": "Rebrandly YouTube channel"
+    }
+
+    requestHeaders = {
+        "Content-type": "application/json",
+        "apikey": config("YOUR_API_KEY"),
+        "workspace": config("YOUR_WORKSPACE_ID")
+    }
+
+    r = requests.post("https://api.rebrandly.com/v1/links",
+                      data=json.dumps(linkRequest),
+                      headers=requestHeaders)
+
+    if (r.status_code == requests.codes.ok):
+        link = r.json()
+        # print("Long URL was %s, short URL is %s" % (link["destination"], link["shortUrl"]))
+        return link["shortUrl"]
